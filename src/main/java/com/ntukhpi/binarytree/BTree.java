306de1ec0b8d443e67513731f3ddd5c3acba3036
Gorbunov Michael <miwagorbi@gmail.com>
2016-12-25 20:41:22 +0200

    Wrote javadoc.

--- src/main/java/com/ntukhpi/binarytree/BTree.java
+++ src/main/java/com/ntukhpi/binarytree/BTree.java
@@ -8,18 +8,41 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
+ * Приложение {@code BTree} является визуальной демонстрацией бинарного дерева поиска.
+ * <br>Разработано в рамках курса "Основы Дисретной Математики" факультета КИТ НТУ "ХПИ".
+ * <br>Визуальная модель позволяет добавлять, удалять, изменять значения в бинарном дереве.
+ *
  * @author Alexander Gorbunov
  */
 public class BTree extends Application {
 
+    /**
+     * Минимальная высота окна в пикселях.
+     */
     private static final int MIN_HEIGHT = 520;
 
+    /**
+     * Минимальная ширина окна в пикселях.
+     */
     private static final int MIN_WIDTH = 300;
 
+    /**
+     * Значок приложения.
+     */
     private static final Image ICON = new Image(BTree.class.getResourceAsStream("/icon.png"));
 
+    /**
+     * Заголовок окна.
+     */
     private static final String TITLE = "Binary Search Tree";
 
+    /**
+     * Метод запуска приложения по основному дейсствию.
+     * Инициализирует компоненты окна, его параметры, загаловок, значок и.т.п.
+     *
+     * @param primaryStage абстракция над главным (и единственным) окном приложения.
+     * @throws Exception любое исключение в зоде выполнения работы приложения.
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/layout.fxml"));
@@ -35,6 +58,11 @@ public class BTree extends Application {
         primaryStage.show();
     }
 
+    /**
+     * Точка входа в программу.
+     *
+     * @param args аргументы командной строки.
+     */
     public static void main(String[] args) {
         launch(args);
     }
--- src/main/java/com/ntukhpi/binarytree/controller/LayoutController.java
+++ src/main/java/com/ntukhpi/binarytree/controller/LayoutController.java
@@ -21,11 +21,15 @@ import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.*;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 /**
+ * Контроллер элементов раскладки "layout.fxml", отображаемых на основном окне.
+ * <br>Инкапсулирует обработку действий пользователя, содержит и управляет визуальным представлением
+ * дерева - обьектом класса {@link BTreeGraph}.
  * @author Alexander Gorbunov
  */
 public class LayoutController implements Initializable {
@@ -67,6 +71,14 @@ public class LayoutController implements Initializable {
      */
     private final NavigableMap<Integer, Integer> cellNumberMap = new TreeMap<>();
 
+    /**
+     * Обьект управления анимацией.
+     * <br>Реализован таким образом, что производит поиск элементов
+     * на визуальном отображении дерева в порядке обхода,
+     * выбирая новый элемент каждые 0.5. секунд.
+     * Когда элементы, задекларированные в {@link LayoutController#cellNumberMap} заканчиваются,
+     * вызывает метод {@link LayoutController#stop()}
+     */
     private final Animation animation = new Timeline(new KeyFrame(
             SELECTION_DURATION,
             actionEvent -> {
@@ -127,6 +139,10 @@ public class LayoutController implements Initializable {
     @FXML
     private ToggleButton sideBarToggle;
 
+    /**
+     * Метод инициализации контроллера, вызываемый после инициалтизации визуальных компонентов.
+     * Служит для вторичной программной настройки компонентов.
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         viewArea.prefWidthProperty().bind(workSpace.widthProperty().subtract(rightControlGroup.getWidth()));
@@ -145,6 +161,9 @@ public class LayoutController implements Initializable {
      ##############################*/
 
 
+    /**
+     * Запустить или продолжить анимацию обхода.
+     */
     @FXML
     public void play() {
         if (!treeGraph.isEmpty()) {
@@ -153,12 +172,18 @@ public class LayoutController implements Initializable {
         }
     }
 
+    /**
+     * Приостановить анимацию обхода.
+     */
     @FXML
     public void pause() {
         animation.pause();
         disableControls(false);
     }
 
+    /**
+     * Остановить и сбросить анимацию обхода.
+     */
     @FXML
     public void stop() {
         animation.stop();
@@ -167,12 +192,18 @@ public class LayoutController implements Initializable {
         initTraversalMode();
     }
 
+    /**
+     * Добавить значение из поля ввода (если оно является целым числом) как новый элемент дерева.
+     */
     @FXML
     public void insert() {
         getInput().ifPresent(treeGraph::addNode);
         updateTraversal();
     }
 
+    /**
+     * Добавить случайное значение как новый элемент дерева.
+     */
     @FXML
     public void insertRandom() {
         RANDOM.setSeed(System.currentTimeMillis());
@@ -181,28 +212,43 @@ public class LayoutController implements Initializable {
         updateTraversal();
     }
 
+    /**
+     * Удалить выделенный элемент дерева.
+     */
     @FXML
     public void remove() {
         treeGraph.removeNode();
         updateTraversal();
     }
 
+    /**
+     * Изменить значение выделенного элемента дерева на значение из поля ввода.
+     */
     @FXML
     public void mutate() {
         getInput().ifPresent(treeGraph::mutateNode);
         updateTraversal();
     }
 
+    /**
+     * Выделить элемент дерева с наименьшим значением.
+     */
     @FXML
     public void findMin() {
         treeGraph.min();
     }
 
+    /**
+     * Выделить элемент дерева с наибольшим значением.
+     */
     @FXML
     public void findMax() {
         treeGraph.max();
     }
 
+    /**
+     * Убрать либо вернуть боковую панель управления.
+     */
     @FXML
     public void hideSideBar() {
         if (!sideBarToggle.isSelected()) {
@@ -212,6 +258,15 @@ public class LayoutController implements Initializable {
         }
     }
 
+    /**
+     * Общий обработчик событий клавиатуры.
+     * <br>По кнопке ENTER - принимает значение из поля ввода.
+     * <br>Если такое значение есть на дереве - выделяет ячейку,
+     * иначе добавляет и ввыделяет новую.
+     * <br>По кнопке DELETE - удаляет віделенную ячейку.
+     *
+     * @param keyEvent событие нажатия на кнопку клавиатуры.
+     */
     @FXML
     public void onKeyPressed(KeyEvent keyEvent) {
         KeyCode keyCode = keyEvent.getCode();
@@ -226,12 +281,18 @@ public class LayoutController implements Initializable {
         }
     }
 
+    /**
+     * Перестроить дерево в сбалансированный вид.
+     */
     @FXML
     public void balance() {
         treeGraph.balance();
         updateTraversal();
     }
 
+    /**
+     * Очистить дерево.
+     */
     @FXML
     public void clean() {
         treeGraph.clear();
@@ -239,18 +300,23 @@ public class LayoutController implements Initializable {
         updateTraversal();
     }
 
+    /**
+     * Завершить работу программы.
+     */
     @FXML
     public void close() {
         Platform.exit();
     }
 
+    /**
+     * Инициализировать один из порядков обхода.
+     * <br>Подсвечивает соответствующую строку в нижней панели.
+     * <br>Заполняет {@link LayoutController#cellNumberMap} значениями из дерева в порядке обхода.
+     */
     @FXML
     public void initTraversalMode() {
-        cellNumberMap.clear();
-
         Integer[] cellValues;
         Text text;
-
         traverseMode = Optional.ofNullable(orders.getSelectedToggle())
                 .orElseGet(() -> {
                     orders.selectToggle(traverseMode);
@@ -270,10 +336,16 @@ public class LayoutController implements Initializable {
             throw new IllegalStateException("One traverse order should always be selected");
         }
         lightUpText(text);
+        cellNumberMap.clear();
         cellNumberMap.putAll(mapArrayValsToPositions(cellValues));
         animation.jumpTo(Duration.ZERO);
     }
 
+    /**
+     * Подсветить текст.
+     *
+     * @param text визуальній компонент "текст".
+     */
     private void lightUpText(Text text) {
         console.getChildren().forEach(node -> node.getStyleClass().
                 removeIf(Predicate.isEqual(Style.CONSOLE_OUT_SELECTED.getStyleClass())));
@@ -289,6 +361,10 @@ public class LayoutController implements Initializable {
         orders.getToggles().forEach(toggle -> ((Node) toggle).setDisable(disable));
     }
 
+    /**
+     * Обновить содержимое нижней панели на основе значений, полученных из {@link LayoutController#treeGraph}.
+     * <br>Также вызывет {@link LayoutController#initTraversalMode()}.
+     */
     private void updateTraversal() {
         preOrderOut.setText(Arrays.stream(treeGraph.getTraversalPreOrder())
                 .map(String::valueOf)
@@ -302,6 +378,10 @@ public class LayoutController implements Initializable {
         initTraversalMode();
     }
 
+    /**
+     * Проверить наличие целого числа в поле ввода и вернуть его внутри контейнера Optional.
+     * @return Optional, возможно содержащий целое число.
+     */
     private Optional<Integer> getInput() {
         String input = this.input.getText();
 
@@ -317,11 +397,17 @@ public class LayoutController implements Initializable {
     }
 
 
+    /**
+     * Утилитарный статический метод для создания структуры данных,
+     * соответствующей спецификации {@link LayoutController#cellNumberMap}
+     *
+     * @param vals массив значений
+     * @param <T> тип-аргумент, представляющий собой любой сравниваемый с самим собой тип.
+     *
+     * @return карту с ключами - индексами значений массива и значениям - соотвевтсятвующими значениями элементов массива.
+     */
     private static <T extends Comparable<T>> Map<Integer, T> mapArrayValsToPositions(T[] vals) {
-        Map<Integer, T> posMap = new TreeMap<>();
-        for (int i = 0; i < vals.length; i++) {
-            posMap.put(i, vals[i]);
-        }
-        return posMap;
+        List<T> asList = Arrays.asList(vals);
+        return asList.stream().collect(Collectors.toMap(asList::indexOf, Function.identity()));
     }
 }
