c9dab45a67e1616e8f68cf4f9e0d8e42b9bcdb7f
Gorbunov Michael <miwagorbi@gmail.com>
2016-12-25 22:29:43 +0200

    Javadoc for TreeFactory

--- src/main/java/com/ntukhpi/binarytree/model/TreeFactory.java
+++ src/main/java/com/ntukhpi/binarytree/model/TreeFactory.java
@@ -3,12 +3,30 @@ package com.ntukhpi.binarytree.model;
 
 import java.util.*;
 
+/**
+ * Фабрика обьектов класса {@link ImmutableBinaryTree} и подклассов.
+ * <br>Предоставляет методы для создания новых бинарных деревьев.
+ */
 public final class TreeFactory {
 
+    /**
+     * Метод инициализации пустого дерева.
+     * @param <U> тип-параметр значений, которые будут хранится в дереве.
+     * @return новое пустое дерево.
+     */
     public final <U extends Comparable<U>> ImmutableBinaryTree<U> immutableTree() {
         return EmptyTree.instance();
     }
 
+    /**
+     * Метод инициализации дерева с указанными значениями.
+     * <br>Значения добавляются в дерево в том порядке, в котором они переданы в метод.
+     * <br>Сбалансированность полученного дерева не гарантируется.
+     *
+     * @param elems массив значений.
+     * @param <U> тип-параметр значений, которые будут хранится в дереве.
+     * @return новое дерево с переданными значениями в вершинах.
+     */
     @SafeVarargs
     public final <U extends Comparable<U>> ImmutableBinaryTree<U> immutableTree(final U... elems) {
         ImmutableBinaryTree<U> tree = immutableTree();
@@ -18,14 +36,33 @@ public final class TreeFactory {
         return tree;
     }
 
+    /**
+     * Метод инициализации сбалансированного дерева с указанными значениями.
+     * <br>Значения сортируются и добавляются в дерево через метод {@link TreeFactory#populateTree(List)}
+     * <br>Сбалансированность полученного дерева гарантируется.
+     *
+     * @param elems массив значений.
+     * @param <U> тип-параметр значений, которые будут хранится в дереве.
+     * @return новое сбалансированное дерево с переданными значениями в вершинах.
+     */
     @SafeVarargs
     public final <U extends Comparable<U>> ImmutableBinaryTree<U> balancedTree(final U... elems) {
-        return populateTree(Arrays.asList(elems));
+        List<U> asList = Arrays.asList(elems);
+        asList.sort(Comparator.naturalOrder());
+        return populateTree(asList);
     }
 
+    /**
+     * Метод принимает отсортированный список значений,
+     * создает новое пустое дерево
+     * и рекурсивно добавляет в него среднее значение списка, потом среднее значение каждой из половин списка
+     * - в корень правого и левого поддеревьев и.т.п., пока не иссякнут значения в списке.
+     *
+     * @param elems отсортированный список значений
+     * @param <U> тип-параметр значений, которые будут хранится в дереве.
+     * @return новое сбалансированное дерево с переданными значениями в вершинах.
+     */
     private <U extends Comparable<U>>ImmutableBinaryTree<U> populateTree(List<U> elems) {
-        elems.sort(Comparator.naturalOrder());
-
         if (elems.isEmpty()) return immutableTree();
 
         ImmutableBinaryTree<U> tree = immutableTree();
